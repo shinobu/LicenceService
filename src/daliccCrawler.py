1dfae7d32866a3d583c455cb65d9ac2c49f0f1e6
shinobu <niehoff.fabian@web.de>
2019-12-10 00:16:06 +0100

    rework crawler for daliccs new layout

--- src/daliccCrawler.py
+++ src/daliccCrawler.py
@@ -4,6 +4,7 @@ import urllib
 import shutil
 import os
 import time
+import json
 logging.getLogger('scrapy').setLevel(logging.WARNING)
 
 
@@ -14,36 +15,69 @@ class daliccCrawler(scrapy.Spider):
         super(daliccCrawler, self).__init__(*args, **kwargs)
         self.start_urls = ['https://dalicc.net/license-library?page=0']
         self.counter = 0
-        self.downloadURLs = []
-        self.scriptDir = os.path.dirname(__file__)
+        self.licenseURLs = []
+        self.scriptDir = os.path.dirname(os.path.abspath(__file__))
+        with open(self.scriptDir + '/config.json') as configFile:
+            self.config = json.load(configFile)
+        self.licenseDir = self.config['path']
+        if not (self.licenseDir[0] == '/') and not (self.licenseDir[1] == ':'):
+            # path is not absolute
+            self.licenseDir = self.scriptDir + '/' + self.licenseDir
+        os.makedirs(self.licenseDir, exist_ok=True)
 
     def parse(self, response):
-        tmpURLs = response.css('a.download-rdf::attr(href)').getall()
-        if tmpURLs[0] not in self.downloadURLs:
-            self.downloadURLs += tmpURLs
+        # even after new page layout, still gather links first, now check for license list elements that arent pagers and strip for hrefs
+        tmpURLs = response.xpath('//div[has-class("license-list")]').xpath('.//li[not(has-class("pager__item"))]').css('a::attr(href)').getall()
+        if len(tmpURLs) > 0 and tmpURLs[0] not in self.licenseURLs:
+            self.licenseURLs += tmpURLs
             self.counter += 1
-            yield response.follow('https://dalicc.net/license-library?page=' + str(self.counter), callback=self.parse)
+            yield response.follow('https://www.dalicc.net/license-library?page=' + str(self.counter), callback=self.parse)
         else:
-            #download all files
-            os.makedirs(self.scriptDir + '/tmp', exist_ok=True)
-            for download in self.downloadURLs:
-                url = 'https://dalicc.net' + download
-                fileName = 'tmp/' + download[17:]
-                filePath = os.path.join(self.scriptDir, fileName)
-                downloaded = self.downloadFile(url, filePath)
-                if not downloaded:
-                    logging.getLogger().debug('failed downloading ' + fileName)
+            for url in self.licenseURLs:
+                yield response.follow('https://www.dalicc.net' + url, callback=self.parse)
+        if '?page=' not in response.url and len(response.xpath("//li[contains(text(),'Dataset')]").getall()) > 0:
+            # files cant be downloaded anymore and need to be extracted now, onlz checks dataset licenses
+            lines = []
+            lines.append('<' + response.url + '> a <http://www.w3.org/ns/odrl/2/Set> .\n')
+            self.fillTextFields(lines, response.xpath("//div[has-class('license-box license-info')]").xpath(".//li[not(.//li)]"), response.url)
+            self.fillPermissions(lines, response.xpath("//div[has-class('license-box license-permissions')]").xpath(".//div[has-class('license-data license-permissions-data')]"), response.url)
+            self.fillProhibitions(lines, response.xpath("//div[has-class('license-box license-prohibitions')]"), response.url)
+            fileName = response.url[39:]
+            filePath = os.path.join(self.licenseDir, fileName)
+            print(filePath)
+            with open(filePath + '.ttl', 'w') as file:
+                file.write(''.join(lines))
 
-    def downloadFile(self, url, filePath):
-        maxRetry = 50
-        downloaded = False
-        while(maxRetry > 0):
-            try:
-                with urllib.request.urlopen(url, timeout=10) as response, open(filePath, 'wb') as outffile:
-                    shutil.copyfileobj(response, outffile)
-                    downloaded = True
-                break
-            except urllib.error.URLError as e:
-                maxRetry = maxRetry - 1
-                time.sleep(10)
-        return downloaded
+    def fillTextFields(self, lines, textFields, uri):
+        for textField in textFields:
+            # text is split through children, fields with links have 3, with text only 2 fields
+            text = textField.xpath(".//text()").getall()
+            if text[0] in 'Source':
+                lines.append('<' + uri + '> <http://purl.org/dc/terms/source> "' + text[2] + '" .\n')
+            if text[0] in 'Publisher':
+                lines.append('<' + uri + '> <http://purl.org/dc/terms/publisher> "' + text[1][3:] + '" .\n')
+
+    def fillPermissions(self, lines, permissionsGroups, uri):
+        # 1 field for permissions without duty (if exists), all other fields for permissions with duties
+        # now share-alike is a duty for dalicc? if existant even the first field gets that duty
+        for permissions in permissionsGroups:
+            lines.append('<' + uri + '> <http://www.w3.org/ns/odrl/2/permission> [ \n')
+            lines.append('a <http://www.w3.org/ns/odrl/2/Permission> ; \n')
+            permissionPart = permissions.xpath("./ul//li/@data-uri").getall()
+            for permission in permissionPart:
+                lines.append('<http://www.w3.org/ns/odrl/2/action> <' + permission + '> ;\n')
+            dutyPart = permissions.xpath("./div/ul//li/@data-uri").getall()
+            if len(dutyPart) > 0:
+                lines.append('<http://www.w3.org/ns/odrl/2/duty> [ \n')
+                lines.append(' a <http://www.w3.org/ns/odrl/2/Duty> ;\n')
+                for duty in dutyPart:
+                    lines.append('<http://www.w3.org/ns/odrl/2/action> <' + duty + '> ;\n')
+                lines.append('] \n')
+            lines.append('] .\n')
+
+    def fillProhibitions(self, lines, prohibitionBox, uri):
+        lines.append('<' + uri + '> <http://www.w3.org/ns/odrl/2/prohibition> [ \n')
+        lines.append('a <http://www.w3.org/ns/odrl/2/Prohibition> ; \n')
+        for prohibition in prohibitionBox.xpath("./ul//li/@data-uri").getall():
+            lines.append('<http://www.w3.org/ns/odrl/2/action> <' + prohibition + '> ;\n')
+        lines.append('] .\n')
--- src/daliccLicenseProcessor.py
+++ src/daliccLicenseProcessor.py
@@ -9,13 +9,12 @@ class daliccLicenseProcessor:
 
     def __init__(self):
         self.scriptDir = os.path.dirname(os.path.abspath(__file__))
-        with open('config.json') as configFile:
+        with open(self.scriptDir + '/config.json') as configFile:
             self.config = json.load(configFile)
         self.licenseDir = self.config['path']
         if not (self.licenseDir[0] == '/') and not (self.licenseDir[1] == ':'):
             # path is not absolute
             self.licenseDir = self.scriptDir + '/' + self.licenseDir
-        print(self.licenseDir)
         self.graph = self.config['daliccGraph']
         self.endpoint = self.config['endpoint']
         self.licenseGraph = UtilitySupplier.downloadLicenseGraph(self, self.endpoint, self.graph)
@@ -31,56 +30,25 @@ class daliccLicenseProcessor:
             raise e
         addTriples = ''
         deleteSubjects = ''
-        for filepath in files:
-            if filepath[-4:] != '.rdf':
+        for filename in files:
+            if filename[-4:] != '.ttl':
                 continue
-            file = open(self.licenseDir + filepath, 'r')
-            fileString = file.read()
-            lines = fileString.splitlines()
-            file.close()
-            if '<odrl:target rdf:resource="http://purl.org/dc/dcmitype/Dataset"/>' in fileString:
-                deletionTargets = ('odrl:target', 'odrl:assigner', 'odrl:AssetCollection', 'odrl:Party', 'dc:creator')
-                saveTargets = ('dc:title', 'dc:alternative', 'cc:attributionName', 'dc:source', 'cc:legalcode')
-                savedLines = ''
-                for i, line in enumerate(lines):
-                    # check for plain deletion of target assigner including asset collections
-                    # they are urns and probably can only partially be deciphered
-                    # save all lines with dc title/anothername in a string with \n's
-                    # those can occasionally? randomly? be in the asset collection instead of the license
-                    # in a second run put them after the set start
-                    if any(deletionTarget in line for deletionTarget in deletionTargets):
-                        lines[i] = ''
-                    if any(saveTarget in line for saveTarget in saveTargets):
-                        savedLines += line + '\n'
-                        lines[i] = ''
-                for i, line in enumerate(lines):
-                    # add savedLines
-                    if 'https://dalicc.net/license-library/' in line:
-                        licenseURI = line[i]
-                        lines[i] += '\n' + savedLines
-                        lines[i] += '<dc:creator>dalicc</dc:creator>\n'
-                        break
-                # >license name exists and is from daliic 
-                # ->compare license graph with file, and if overlap is not 100%
-                # --> change delete differences, insert whole file/only changes?
-                # --> else ignore
-                # > add whole file
-                if (licenseURI, None, None) not in self.licenseGraph:
-                    # query needs to insert all triples, no checks required
-                    tmpGraph = rdflib.graph.Graph()
-                    tmpGraph.parse(data=''.join(lines), format='xml')
-                    for triple in tmpGraph.query('CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}'):
-                        if isinstance(triple[0], rdflib.term.URIRef):
-                            addTriples += '<' + str(triple[0]) + '> '
-                        else:
-                            addTriples += '_:' + str(triple[0]) + ' '
-                        addTriples += '<' + str(triple[1]) + '> '
-                        if isinstance(triple[2], rdflib.term.URIRef):
-                            addTriples += '<' + str(triple[2]) + '> .\n'
-                        elif isinstance(triple[2], rdflib.term.Literal):
-                            addTriples += '"' + str(triple[2]).replace('"', '\\"') + '" .\n'
-                        else:
-                            addTriples += '_:' + str(triple[2]) + ' .\n'
+            if ('https://www.dalicc.net/license-library' + filename, None, None) not in self.licenseGraph:
+                # query needs to insert all triples, no checks required
+                tmpGraph = rdflib.graph.Graph()
+                tmpGraph.parse(self.licenseDir + filename, format='turtle')
+                for triple in tmpGraph.query('CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}'):
+                    if isinstance(triple[0], rdflib.term.URIRef):
+                        addTriples += '<' + str(triple[0]) + '> '
+                    else:
+                        addTriples += '_:' + str(triple[0]) + ' '
+                    addTriples += '<' + str(triple[1]) + '> '
+                    if isinstance(triple[2], rdflib.term.URIRef):
+                        addTriples += '<' + str(triple[2]) + '> .\n'
+                    elif isinstance(triple[2], rdflib.term.Literal):
+                        addTriples += '"' + str(triple[2]).replace('"', '\\"') + '" .\n'
+                    else:
+                        addTriples += '_:' + str(triple[2]) + ' .\n'
                 # not as relevant with dalicc licenses having their own graph
                 # not working as intended, because rdflib cant deal with
                 # insert/delte queries with OPTIONAL (valid SPARQL) atm
